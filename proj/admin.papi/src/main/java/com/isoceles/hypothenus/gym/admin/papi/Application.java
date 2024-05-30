be9d4183553ff2df27600a2b4ee6b4095e0031e4
fortinb <fortinb.perso@gmail.com>
2024-05-30 10:01:17 -0400

    Initial commit

---
+++ proj/admin.papi/src/main/java/com/isoceles/hypothenus/gym/admin/papi/Application.java
@@ -0,0 +1,13 @@
+package com.isoceles.hypothenus.gym.admin.papi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
